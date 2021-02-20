0f52382b138388e403dc1782ee0c14708c4615cf
Naval Verma <navalverma999@gmail.com>
2021-02-20 21:18:22 +0530

    firebase and redux structure added

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,8 @@
-export default () => {
-    return { name: 'Priya Verma' }
-}
+import { combineReducers } from "redux";
+import authReducer from './auth.reducer';
+
+const rootReducer = combineReducers({
+    auth: authReducer
+})
+
+export default rootReducer;
